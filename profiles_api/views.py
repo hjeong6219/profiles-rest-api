8e627527c1e9a760cdd024a06d7ba983595d87fb
Josh <it.josh.jeong@gmail.com>
2022-12-17 12:57:27 -0500

    Added user profile feed API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,10 +6,12 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -52,6 +54,7 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
+
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
@@ -97,6 +100,7 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
         return Response({'httpd_method': 'DELETE'})
 
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and upodating profiles"""
     serializer_class = serializers.UserProfileSerializer
@@ -111,3 +115,14 @@ class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading, and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+    
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
